68eea12e32eb5ec57137666e119aa4422bf8350d
Thiago-Rocketseat <thiago-rocketseat@pop-os.localdomain>
2021-01-06 17:18:58 -0300

    Desafio finalizado

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
+import api from '../../services/api';
+
+
 import { Container } from './styles';
 
 interface IFoodPlate {
@@ -27,11 +30,21 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    try {
+      await api.put(`/foods/${food.id}`, {
+        ...food,
+        available: !isAvailable
+      })
+
+      setIsAvailable(!isAvailable)
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   function setEditingFood(): void {
-    // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
